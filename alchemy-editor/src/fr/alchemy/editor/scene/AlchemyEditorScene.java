46b29f123bb9a80c4464a7e2c762d8d4ff8fbce8
GnosticOccultist <41146201+GnosticOccultist@users.noreply.github.com>
2018-10-14 13:36:05 +0200

    Started the editor.
    Added the AlchemyEditor application and scene.

---
+++ alchemy-editor/src/fr/alchemy/editor/scene/AlchemyEditorScene.java
@@ -0,0 +1,11 @@
+package fr.alchemy.editor.scene;
+
+import fr.alchemy.core.AlchemyApplication;
+import fr.alchemy.core.scene.AlchemyScene;
+
+public class AlchemyEditorScene extends AlchemyScene {
+
+	public AlchemyEditorScene(AlchemyApplication application) {
+		super(application);
+	}
+}
