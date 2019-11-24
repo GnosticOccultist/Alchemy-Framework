0be5405f89e8a4818068afa97b2ea816f75e43ac
GnosticOccultist <41146201+GnosticOccultist@users.noreply.github.com>
2019-11-24 17:16:06 +0100

    Added basic resizing and moving of nodes
    * The nodes can now be resized or moved depending on the mouse cursor position.
    * The mouse cursor is changed depending on what action is performed.

--- alchemy-editor/api.src/fr/alchemy/editor/api/ui/component/nodes/VisualNodeElement.java
+++ alchemy-editor/api.src/fr/alchemy/editor/api/ui/component/nodes/VisualNodeElement.java
@@ -3,7 +3,11 @@ package fr.alchemy.editor.api.ui.component.nodes;
 import java.util.UUID;
 
 import fr.alchemy.utilities.Validator;
+import javafx.geometry.Point2D;
+import javafx.scene.Cursor;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
@@ -17,6 +21,11 @@ import javafx.scene.layout.VBox;
 public class VisualNodeElement<E> extends VBox {
 
 	/**
+	 * The distance resize threshold where the node element can be resized from its bounds.
+	 */
+	private static final int RESIZE_THRESHOLD = 6;
+	
+	/**
 	 * The unique identifier of the node.
 	 */
 	protected final UUID id;
@@ -28,6 +37,18 @@ public class VisualNodeElement<E> extends VBox {
 	 * The parameters container of the node.
 	 */
 	protected final VBox parametersContainer;
+	/**
+	 * The X and Y local coordinates of the node.
+	 */
+	protected double x, y;
+	/**
+	 * The last mouse X and Y local coordinates.
+	 */
+	protected double mouseX, mouseY;
+	/**
+	 * The last resized region of the node.
+	 */
+	private ResizeRegion lastResizeRegion = ResizeRegion.OUTSIDE;
 	
 	/**
 	 * Instantiates a new <code>VisualNodeElement</code> to represent the provided element.
@@ -40,6 +61,11 @@ public class VisualNodeElement<E> extends VBox {
 		this.element = element;
 		this.id = UUID.randomUUID();
 		this.parametersContainer = new VBox();
+		
+		setOnMouseMoved(this::processMouseMoved);
+		setOnMousePressed(this::processMousePressed);
+		setOnMouseDragged(this::processMouseDragged);
+		setOnMouseDragExited(this::processMouseExited);
 		
 		setPrefWidth(200D);
 		getStyleClass().add("visual-node");
@@ -71,6 +97,151 @@ public class VisualNodeElement<E> extends VBox {
 	 * @param container The container of the parameters (not null).
 	 */
 	protected void constructParameters(VBox container) {}
+	
+	/**
+	 * Process the given moved {@link MouseEvent} by changing the {@link Cursor} according to the hovered region 
+	 * of the <code>VisualNodeElement</code>.
+	 * 
+	 * @param event The mouse event that occured (not null).
+	 */
+	protected void processMouseMoved(MouseEvent event) {
+		Validator.nonNull(event, "The mouse event can't be null!");
+		
+		if(event.isPrimaryButtonDown()) {
+			return;
+		}
+		
+		ResizeRegion region = getResizedRegion(event);
+		setCursor(region.getFXCursor());
+	}
+	
+	/**
+	 * Process the given exited {@link MouseEvent} by changing the {@link Cursor} according to the one previously
+	 * set by the parent node.
+	 * 
+	 * @param event The mouse event that occured (not null).
+	 */
+	protected void processMouseExited(MouseEvent event) {
+		Validator.nonNull(event, "The mouse event can't be null!");
+		
+		if(!event.isPrimaryButtonDown()) {
+			setCursor(null);
+		}
+	}
+	
+	/**
+	 * Process the given dragged {@link MouseEvent} by moving the <code>VisualNodeElement</code> according to the mouse delta
+	 * from previously invoked events.
+	 * 
+	 * @param event The mouse event that occured (not null).
+	 */
+	protected void processMouseDragged(MouseEvent event) {
+		Validator.nonNull(event, "The mouse event can't be null!");
+		
+		if(canResize(lastResizeRegion)) {
+			double mouseX = event.getX();
+			setPrefWidth(getPrefWidth() + (mouseX - x));
+			double mouseY = event.getY();
+			setPrefHeight(getPrefHeight() + (mouseY - y));
+			double layoutX = getLayoutX();
+			double layoutY = getLayoutY();
+	        setLayoutX(-1D);
+	        setLayoutY(-1D);
+	        setLayoutX(layoutX);
+	        setLayoutY(layoutY);
+			this.x = mouseX;
+			this.y = mouseY;
+		} else {
+			// Get the local location of the mouse.
+			Point2D posInParent = getParent().sceneToLocal(event.getSceneX(), event.getSceneY());
+			double offsetX = posInParent.getX() - mouseX;
+			double offsetY = posInParent.getY() - mouseY;
+				
+			this.x = x + offsetX;
+			this.y = y + offsetY;
+				
+			setLayoutX(x);
+			setLayoutY(y);
+				
+			// Once the layout has been changed, update the mouse X and Y position for next call.
+			this.mouseX = posInParent.getX();
+			this.mouseY = posInParent.getY();
+		}
+		
+		event.consume();
+	}
+	
+	/**
+	 * Process the given pressed {@link MouseEvent} by saving the mouse and the <code>VisualNodeElement</code> local coordinates,
+	 * for later dragging events.
+	 * 
+	 * @param event The mouse event that occured (not null).
+	 */
+	protected void processMousePressed(MouseEvent event) {
+		Validator.nonNull(event, "The mouse event can't be null!");
+		
+		if(event.getButton() != MouseButton.PRIMARY) {
+			return;
+		}
+		
+		this.lastResizeRegion = getResizedRegion(event);
+		
+		if(canResize(lastResizeRegion)) {
+			this.x = event.getX();
+			this.y = event.getY();
+		} else {
+			Point2D posInParent = getParent().sceneToLocal(event.getSceneX(), event.getSceneY());
+			
+			// Record the current mouse X and Y position of the node
+			this.mouseX = posInParent.getX();
+			this.mouseY = posInParent.getY();
+			
+			this.x = getLayoutX();
+			this.y = getLayoutY();
+			
+			toFront();
+		}
+		
+		event.consume();
+	}
+	
+	/**
+	 * Return whether the <code>VisualNodeElement</code> can be resized by the provided {@link ResizeRegion}.
+	 * 
+	 * @param resizedRegion The region from which a resize has been requested (not null).
+	 * @return				Whether the node element can be resized with the region.
+	 */
+	protected boolean canResize(ResizeRegion resizedRegion) {
+		Validator.nonNull(resizedRegion, "The resized region can't be null!");
+		return resizedRegion != ResizeRegion.OUTSIDE && resizedRegion != ResizeRegion.INSIDE;
+	}
+	
+	/**
+	 * Return the {@link ResizeRegion} from which the {@link MouseEvent} can resize the <code>VisualNodeElement</code>.
+	 * 
+	 * @param event The mouse event containing the mouse position (not null).
+	 * @return		The region from which a resize can be performed.
+	 */
+	private ResizeRegion getResizedRegion(MouseEvent event) {
+		Validator.nonNull(event, "The mouse event can't be null!");
+		
+		double x = event.getX();
+		double y = event.getY();
+		double width = getWidth();
+		double height = getHeight();
+		
+		// Check first if we're totally outisde to avoid useless checks.
+		if(x < 0 || y < 0 || x > width || y > height) {
+			return ResizeRegion.OUTSIDE;
+		}
+		
+		boolean north = y < RESIZE_THRESHOLD;
+		boolean south = y > height - RESIZE_THRESHOLD;
+		boolean east = x > width - RESIZE_THRESHOLD;
+		boolean west = x < RESIZE_THRESHOLD;
+		
+		return ResizeRegion.get(north, south, east, west, true);
+	}
 
 	/**
 	 * Return the title text of the <code>VisualNodeElement</code>, which is displayed
@@ -80,5 +251,126 @@ public class VisualNodeElement<E> extends VBox {
 	 */
 	protected String getTitleText() {
 		return "Title";
+	}
+	
+	/**
+	 * <code>ResizeRegion</code> is an enumeration of possible resizable regions around the border
+	 * of a rectangular shape.
+	 * <p>
+	 * It is used during mouse hover and drag events on the {@link VisualNodeElement} to determine
+	 * resize direction and what cursor is to display during resizing.
+	 * 
+	 * @see VisualNodeElement#changeCursor(MouseEvent)
+	 * 
+	 * @author GnosticOccultist
+	 */
+	public enum ResizeRegion {
+		/**
+		 * The north side of the rectangular shape.
+		 */
+		NORTH,
+		/**
+		 * The south side of the rectangular shape.
+		 */
+		SOUTH,
+		/**
+		 * The east side of the rectangular shape.
+		 */
+		EAST,
+		/**
+		 * The west side of the rectangular shape.
+		 */
+		WEST,
+		/**
+		 * The north-east side of the rectangular shape.
+		 */
+		NORTH_EAST,
+		/**
+		 * The north-west side of the rectangular shape.
+		 */
+		NORTH_WEST,
+		/**
+		 * The south-east side of the rectangular shape.
+		 */
+		SOUTH_EAST,
+		/**
+		 * The south-west side of the rectangular shape.
+		 */
+		SOUTH_WEST,
+		/**
+		 * The inside of the rectangular shape.
+		 */
+		INSIDE,
+		/**
+		 * The outside of the rectangular shape.
+		 */
+		OUTSIDE;
+		
+		/**
+		 * Return the type of {@link Cursor} corresponding to the <code>ResizeRegion</code>.
+		 * 
+		 * @return The type of cursor corresponding to the resize region, or null if the resize region 
+		 * 		   is inside or outside the resize margin.
+		 */
+		public Cursor getFXCursor() {
+			switch (this) {
+	            case NORTH_EAST:
+	                return Cursor.NE_RESIZE;
+	            case NORTH_WEST:
+	            	return Cursor.NW_RESIZE;
+	            case SOUTH_EAST:
+	            	return Cursor.SE_RESIZE;
+	            case SOUTH_WEST:
+	            	return Cursor.SW_RESIZE;
+	            case NORTH:
+	                return Cursor.N_RESIZE;
+	            case SOUTH:
+	                return Cursor.S_RESIZE;
+	            case EAST:
+	                return Cursor.E_RESIZE;
+	            case WEST:
+	            	return Cursor.W_RESIZE;
+	
+	            case INSIDE:
+	            case OUTSIDE:
+	                // Set to null instead of Cursor.DEFAULT so it doesn't overwrite cursor settings of parent.
+	            	return null;
+	        }
+			return null;
+		}
+		
+		/**
+		 * Return a <code>ResizeRegion</code> matching the provided boolean values.
+		 * 
+		 * @param north  Whether the north region is focused.
+		 * @param south  Whether the south region is focused.
+		 * @param east   Whether the east region is focused.
+		 * @param west   Whether the west region is focused.
+		 * @param inside Whether the inside region is focused.
+		 * @return		 The resize region matching the boolean (not null).
+		 */
+		public static ResizeRegion get(boolean north, boolean south, boolean east, boolean west, boolean inside) {
+			if(north && east) {
+	        	return ResizeRegion.NORTH_EAST;
+	        } else if (north && west) {
+	            return ResizeRegion.NORTH_WEST;
+	        } else if (south && east) {
+	            return ResizeRegion.SOUTH_EAST;
+	        } else if (south && west) {
+	            return ResizeRegion.SOUTH_WEST;
+	        } else if (north) {
+	            return ResizeRegion.NORTH;
+	        } else if (south) {
+	            return ResizeRegion.SOUTH;
+	        } else if (east) {
+	            return ResizeRegion.EAST;
+	        } else if (west) {
+	            return ResizeRegion.WEST;
+	        } else if (inside) {
+	        	return ResizeRegion.INSIDE;
+	        }
+			
+			return ResizeRegion.OUTSIDE;
+		}
 	}
 }
--- alchemy-editor/api.src/fr/alchemy/editor/api/ui/component/nodes/VisualNodesContainer.java
+++ alchemy-editor/api.src/fr/alchemy/editor/api/ui/component/nodes/VisualNodesContainer.java
@@ -1,16 +1,22 @@
 package fr.alchemy.editor.api.ui.component.nodes;
 
+import fr.alchemy.utilities.array.Array;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Pane;
 
 public class VisualNodesContainer<E extends VisualNodeElement> extends ScrollPane {
 
 	/**
+	 * The current nodes elements present in the container.
+	 */
+	protected final Array<E> nodeElements;
+	/**
 	 * The root pane of the nodes container.
 	 */
-	private final Pane root;
+	protected final Pane root;
 
 	public VisualNodesContainer() {
+		this.nodeElements = Array.ofType(VisualNodeElement.class);
 		this.root = new Pane();
 		this.root.prefWidthProperty().bind(widthProperty());
 		this.root.prefHeightProperty().bind(heightProperty());
@@ -18,8 +24,8 @@ public class VisualNodesContainer<E extends VisualNodeElement> extends ScrollPan
 		root.getStyleClass().add("visual-nodes-root");
 		
 		setPannable(true);
-		setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
-		setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
+		setHbarPolicy(ScrollBarPolicy.NEVER);
+		setVbarPolicy(ScrollBarPolicy.NEVER);
 		setContent(root);
 		setFitToHeight(true);
 		setFitToWidth(true);
@@ -28,12 +34,16 @@ public class VisualNodesContainer<E extends VisualNodeElement> extends ScrollPan
 	public void add(E node) {
 		root.getChildren().add(node);
 		
+		nodeElements.add(node);
+		
 		node.setLayoutX(10D);
 		node.setLayoutY(10D);
 	}
 	
 	public void add(int index, E node) {
 		root.getChildren().add(index, node);
+		
+		nodeElements.add(node);
 		
 		node.setLayoutX(10D);
 		node.setLayoutY(10D);
